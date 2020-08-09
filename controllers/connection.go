93023e0519069a675a133c6d0c5148f74c9c1e40
kooi nam ng <kooinamng@koois-MacBook-Pro.local>
2020-08-09 23:08:43 +0800

    change accessor for params and property for connection with fallback

--- controllers/connection.go
+++ controllers/connection.go
@@ -8,14 +8,14 @@ import (
 type Connection struct {
 	conn       socketio.Conn
 	properties map[string]interface{}
-	Params     map[string]interface{}
+	params     map[string]interface{}
 }
 
 // MakeConnection use to instantiate connection instance
 func MakeConnection(conn socketio.Conn, params map[string]interface{}) *Connection {
 	connection := &Connection{
 		conn:       conn,
-		Params:     params,
+		params:     params,
 		properties: make(map[string]interface{}),
 	}
 
@@ -27,17 +27,29 @@ func (connection *Connection) Join(room string) {
 	connection.conn.Join(room)
 }
 
-// GetQueryParams used to get query params
-func (connection *Connection) GetQueryParams(query string) string {
-	url := connection.conn.URL()
-
-	return url.Query().Get(query)
-}
-
-func (connection *Connection) AddProperty(key string, value interface{}) {
+// SetProperty used to set property that can used across the context
+func (connection *Connection) SetProperty(key string, value interface{}) {
 	connection.properties[key] = value
 }
 
-func (connection *Connection) GetProperty(key string) interface{} {
-	return connection.properties[key]
+// Property used to retrieve context property value with a fallback
+func (connection *Connection) Property(key string, fallback interface{}) interface{} {
+	value := connection.properties[key]
+
+	if value != nil {
+		return value
+	}
+
+	return fallback
+}
+
+// Params used to retrieve params property value with a fallback
+func (connection *Connection) Params(key string, fallback interface{}) interface{} {
+	value := connection.properties[key]
+
+	if value != nil {
+		return value
+	}
+
+	return fallback
 }
